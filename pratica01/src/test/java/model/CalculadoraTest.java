87e9220e42cc71113359e7e9ad54d8987820d141
Leticia Prudente <let.prudente@gmail.com>
2021-03-28 22:14:22 -0300

    Fase Vermelha: Cálculo da Reversa não implementado

--- pratica01/src/test/java/model/CalculadoraTest.java
+++ pratica01/src/test/java/model/CalculadoraTest.java
@@ -2,45 +2,40 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest{
 	
-	/*@Test
+	@Test
 	public void inversoTest() throws Exception {
 		Calculadora calc = new Calculadora();
 		float resultado = calc.inverso(2);
 		assertEquals(0.5, resultado);
-	}*/
+	}
+	
+	/*
+	@Test
+	public void inversoTestNegativo() throws Exception {
+		Calculadora calc = new Calculadora();
+		float resultado = calc.inverso(-4);
+		assertEquals(-0.25, resultado);
+	}
 	
 	@Test
-	public void inversoTestZero(){
-	/*	
-		Throwable exception = assertThrows(
-            IllegalArgumentException.class, () -> {
-                Calculadora calc = new Calculadora();
-				float resultado = calc.inverso(0);
-            }
-    );
- 
-    assertEquals("Nao e possível realizar divisao por zero", exception.getMessage()); 
-	*/	
+	public void inversoTestZero() {
 		try {
 			Calculadora calc = new Calculadora();
 			float resultado = calc.inverso(0);
-			Fail("O teste falhou");
+			fail("O teste falhou");
 		} 
 		catch (Exception error) {
 			assertEquals( "Nao e possível realizar divisao por zero!", error.getMessage());
 		}
 
 	}
+	*/
 	
-	/*@Test
-	public void inversoTestNegativo(){
-		Calculadora calc = new Calculadora();
-		float resultado = calc.inverso(-2);
-		assertEquals(-0.5, resultado);
-	}*/
-	
+
 }
