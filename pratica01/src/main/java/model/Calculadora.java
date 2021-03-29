d265c761adf8ae3169874907d0cd0cfee6e3b911
Leticia Prudente <let.prudente@gmail.com>
2021-03-28 22:25:50 -0300

    Fase Verde: Inverso Implementado e Funcional

--- pratica01/src/main/java/model/Calculadora.java
+++ pratica01/src/main/java/model/Calculadora.java
@@ -2,10 +2,9 @@ package model;
 
 public class Calculadora{
 	public float inverso(int x)  throws Exception {
-		/*
-		if(x == 0){
+		/*if(x == 0){
 			throw new Exception("Nao e possível realizar divisao por zero!");	
 		}*/
-		return (x); //(float) 1 )/((float) x
+		return ((float) 1 )/((float) x);
 	}	
 }
