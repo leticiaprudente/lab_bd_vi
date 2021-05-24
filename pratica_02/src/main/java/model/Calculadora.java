3f1746ca46bbf2f92b00f94664f7b8a7afa32983
Leticia Prudente <let.prudente@gmail.com>
2021-05-23 23:05:52 -0300

    Pratica 02 - Fase Verde (Final)

--- pratica_02/src/main/java/model/Calculadora.java
+++ pratica_02/src/main/java/model/Calculadora.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 public class Calculadora{
 	public BigDecimal inverso(int x)  throws Exception {
 		//FASE VERDE 2
-		/*if(x == 0){ 
+		if(x == 0){ 
 			throw new Exception("Nao e possivel realizar divisao por zero!");	
 		}
 		else if(x>=1 && x<=10000){
@@ -16,7 +16,7 @@ public class Calculadora{
 		}
 		else{
 			throw new Exception("O valor inserido esta fora do limite esperado");	
-		}*/
+		}
 
 		return ((float) 1 ) / ((float) x) ; 
 	}	
