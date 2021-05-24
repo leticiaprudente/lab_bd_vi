8d60760c73888b3034299f5d6a570fe08d584612
Leticia Prudente <let.prudente@gmail.com>
2021-05-24 20:23:04 -0300

    [Correção DataType] Prática 02 - Fase Verde

--- pratica_02/src/main/java/model/Calculadora.java
+++ pratica_02/src/main/java/model/Calculadora.java
@@ -3,7 +3,7 @@ import java.math.BigDecimal;
 
 public class Calculadora{
 	public BigDecimal inverso(int x)  throws Exception {
-		/*
+	
 		if(x == 0){ 
 			throw new Exception("Nao e possivel realizar divisao por zero!");	
 		}
@@ -17,13 +17,13 @@ public class Calculadora{
 		else{
 			throw new Exception("O valor inserido esta fora do limite esperado");	
 		}
-		*/
-		
+
+		/*
 		BigDecimal y = new BigDecimal(x);
 		BigDecimal um = new BigDecimal(1);
 		BigDecimal retorno = um.divide(y);
 		
-
 		return retorno; //new BigDecimal(0);
+		*/
 	}	
 }
